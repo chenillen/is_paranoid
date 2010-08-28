188dbd7333b0d4ffed16134d82614a0e96e851b9
chenillen <chenillen@gmail.com>
2010-08-28 16:16:13 +0800

    ActiveRecord Extended

--- lib/is_paranoid.rb
+++ lib/is_paranoid.rb
@@ -107,32 +107,38 @@ module IsParanoid
   end
 end
 
-##  class ModelName < ActiveRecord::Base
-##    is_paranoid
-##    default_scope :order => "#{ModelName.quoted_table_name}.name"
-##    allows_multiple_default_scopes # this calls alias_method_chain and hooks into our default scoping etc.
-##  end
+
 module MultipleDefaultScopes
-  def allows_multiple_default_scopes
-    self.class_eval do
-      class << self
-        def default_scoping_with_deep_merge
-          returning [] do |default_scoping|
-            merged_default_scopes = {}
-            
-            default_scoping_without_deep_merge.each do |scope|
-              merged_default_scopes = merged_default_scopes.deep_merge(scope)
-            end
-            
-            default_scoping << merged_default_scopes
-          end
-        end
+  extend ActiveRecord::Base
+  
+  module ClassMethods
+    ##  class ModelName < ActiveRecord::Base
+    ##    is_paranoid
+    ##    default_scope :order => "#{ModelName.quoted_table_name}.name"
+    ##    allows_multiple_default_scopes # this calls alias_method_chain and hooks into our default scoping etc.
+    ##  end
+    def allows_multiple_default_scopes
+      class_eval do
         
-        alias_method_chain :default_scoping, :deep_merge
+        class << self
+          def default_scoping_with_deep_merge
+            returning [] do |default_scope|
+              merged_default_scopes = {}
+
+              default_scoping_without_deep_merge.each do |scope|
+                merged_default_scopes = merged_default_scopes.deep_merge(scope)
+              end
+
+              default_scoping << merged_default_scopes
+            end
+          end
+
+          alias_method_chain :default_scoping, :deep_merge
+        end
       end
     end
   end
 end
 
 ActiveSupport.on_load(:active_record) { include IsParanoid }
-ActiveSupport.on_load(:active_record) { include MultipleDefaultScopes }
+ActiveRecord::Base.extend MultipleDefaultScopes
