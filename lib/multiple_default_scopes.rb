4098fe1d81834ae077c126ee77be7c55a7922b39
chenillen <chenillen@gmail.com>
2010-08-28 16:42:38 +0800

    default_scoping or default_scope

--- lib/multiple_default_scopes.rb
+++ lib/multiple_default_scopes.rb
@@ -14,19 +14,19 @@ module MultipleDefaultScopes
       class_eval do
         
         class << self
-          def default_scoping_with_deep_merge
+          def default_scope_with_deep_merge
             returning [] do |default_scope|
               merged_default_scopes = {}
 
-              default_scoping_without_deep_merge.each do |scope|
+              default_scope_without_deep_merge.each do |scope|
                 merged_default_scopes = merged_default_scopes.deep_merge(scope)
               end
 
-              default_scoping << merged_default_scopes
+              default_scope << merged_default_scopes
             end
           end
 
-          alias_method_chain :default_scoping, :deep_merge
+          alias_method_chain :default_scope, :deep_merge
         end
       end
     end
