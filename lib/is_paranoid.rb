80665aae362080cf40ea6518983d657878e96605
chenillen <chenillen@gmail.com>
2010-08-28 15:54:23 +0800

    Added default_scoping_with_deep_merge method

--- lib/is_paranoid.rb
+++ lib/is_paranoid.rb
@@ -107,4 +107,32 @@ module IsParanoid
   end
 end
 
+##  class ModelName < ActiveRecord::Base
+##    is_paranoid
+##    default_scope :order => "#{ModelName.quoted_table_name}.name"
+##    allows_multiple_default_scopes # this calls alias_method_chain and hooks into our default scoping etc.
+##  end
+module MultipleDefaultScopes
+  def allows_multiple_default_scopes
+    self.class_eval do
+      class << self
+        def default_scoping_with_deep_merge
+          returning [] do |default_scoping|
+            merged_default_scopes = {}
+            
+            default_scoping_without_deep_merge.each do |scope|
+              merged_default_scopes = merged_default_scopes.deep_merge(scope)
+            end
+            
+            default_scoping << merged_default_scopes
+          end
+        end
+        
+        alias_method_chain :default_scoping, :deep_merge
+      end
+    end
+  end
+end
+
 ActiveSupport.on_load(:active_record) { include IsParanoid }
+ActiveRecord::Base.extend MultipleDefaultScopes
